78cd0d8b815a04ae22e114635e62702353221e10
Al Fahad <31186116+codebenderAlfah@users.noreply.github.com>
2020-04-20 16:26:28 +0600

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -10,7 +10,7 @@ class Connection{
     function getConnection(){
         $host       = "localhost";
         $username   = "root";
-        $password   = "151151";
+        $password   = "";
         $dbname     = "quarintined";
         try{
             $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
