39d117bcaaaf47fba4e09ffcd90c9868541aced0
Proshanta Kumer Das <57039497+ProshantaKumerDas@users.noreply.github.com>
2020-04-23 23:26:18 +0600

    Update user data.php

--- user data.php
+++ user data.php
@@ -9,8 +9,10 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<script src="js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+		
 		<style>
 		html {
 			font-family: Arial;
@@ -43,10 +45,15 @@
 		ul.topnav li a.active {background-color: #333;}
 
 		ul.topnav li.right {float: right;}
+		
 
-		@media screen and (max-width: 1300px) {
-			ul.topnav li.right, 
-			ul.topnav li {float: none;}
+		.topnav input[type=text] {
+		float: right;
+		padding: 5px;
+		margin-top: 7px;
+		margin-right: 16px;
+		border: none;
+		font-size: 17px;
 		}
 		
 		.table {
@@ -70,6 +77,7 @@
 			<li><a href="registration.php">Registration</a></li>
 			<!-- <li><a href="read tag.php">Scan Card</a></li> -->
 			<li><a href="live_chart.php">Live Data</a></li>
+			<input type="text" placeholder="Search">
 		</ul>
 		<h2>User Information</h2>
 		<div class="container">
