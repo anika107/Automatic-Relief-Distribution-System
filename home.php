03722fb13284b560fa77619c36988bf9cf127825
Proshanta Kumer Das <57039497+ProshantaKumerDas@users.noreply.github.com>
2020-04-23 23:03:17 +0600

    Update home.php

--- home.php
+++ home.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<script src="js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 		<style>
 		html {
 			font-family: Arial;
@@ -19,8 +20,8 @@
 			margin: auto;
 			padding: 0;
 			overflow: hidden;
-			background-color: #4CAF50;
-			width: 70%;
+			background-color: #1abc9c;
+			width: 100%;
 		}
 
 		ul.topnav li {float: left;}
@@ -33,38 +34,88 @@
 			text-decoration: none;
 		}
 
-		ul.topnav li a:hover:not(.active) {background-color: #3e8e41;}
+		ul.topnav li a:hover:not(.active) {background-color: #333;}
 
 		ul.topnav li a.active {background-color: #333;}
 
 		ul.topnav li.right {float: right;}
 
-		@media screen and (max-width: 600px) {
-			ul.topnav li.right, 
-			ul.topnav li {float: none;}
+		.topnav input[type=text] {
+		float: right;
+		padding: 5px;
+		margin-top: 7px;
+		margin-right: 16px;
+		border: none;
+		font-size: 17px;
 		}
+
 		
-		img {
-			display: block;
-			margin-left: auto;
-			margin-right: auto;
-		}
 		</style>
 		
 		<title>Home</title>
 	</head>
 	
 	<body>
-		<h2>Vendor Machine User Database</h2>
+		<br>
 		<ul class="topnav">
 			<li><a class="active" href="home.php">Home</a></li>
 			<li><a href="user data.php">User Data</a></li>
 			<li><a href="registration.php">Registration</a></li>
 			<!-- <li><a href="read tag.php">Scan Card</a></li> -->
+			<li><a href="live_chart.php">Live Data</a></li>
+			<input type="text" placeholder="Search">
 		</ul>
-		<br>
-		<h3>Welcome to Vendor Machine's World</h3>
 		
-		<img src="home.png" alt="" style="width:55%;">
+		<h2 align="center">Vending Machine User Home Page</h2>
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+			<img src="slide1.jpg" alt="Covid-19"  style="width:100%;">
+			<div class="carousel-caption text-dark">
+			<h3>CORONAVIRUS COVID-19</h3>
+			<p>Stay Home & Stay Safe</p>
+			</div>
+
+			</div>
+
+			<div class="item">
+			<img src="slide2.jpg" alt="DNA"  style="width:100%;">
+			<div class="carousel-caption">
+			<h3>CORONAVIRUS COVID-19</h3>
+			<p>Stay Home & Stay Safe</p>
+			</div>
+
+			</div>
+
+			<div class="item">
+			<img src="slide3.PNG" alt="Corona"  style="width:100%;">
+			<div class="carousel-caption">
+			<h3>CORONAVIRUS COVID-19</h3>
+			<p>Stay Home & Stay Safe</p>
+			</div>
+
+			</div>
+		</div>
+
+  <!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		</div>
+		<h3>Vending Machine's World</h3>
+		<br>
 	</body>
 </html>
