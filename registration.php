b949c80b815f5ea0e6ddf8f91758f5001f7f1315
Proshanta Kumer Das <57039497+ProshantaKumerDas@users.noreply.github.com>
2020-04-23 23:05:09 +0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,24 +4,22 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/w3css/3/w3.css">
 		<script src="js/bootstrap.min.js"></script>
 		<script src="jquery.min.js"></script>
-		<script>
-			$(document).ready(function(){
-				 $("#getUID").load("UIDContainer.php");
-				setInterval(function() {
-					$("#getUID").load("UIDContainer.php");
-				}, 500);
-			});
-		</script>
 		
 		<style>
+		
 		html {
 			font-family: Arial;
 			display: inline-block;
 			margin: 0px auto;
 		}
+		body {font-family: Arial, Helvetica, sans-serif;}
+		* {box-sizing: border-box;}
 		
 		textarea {
 			resize: none;
@@ -32,8 +30,8 @@
 			margin: auto;
 			padding: 0;
 			overflow: hidden;
-			background-color: #4CAF50;
-			width: 70%;
+			background-color: #1abc9c;
+			width: 100%;
 		}
 
 		ul.topnav li {float: left;}
@@ -46,15 +44,57 @@
 			text-decoration: none;
 		}
 
-		ul.topnav li a:hover:not(.active) {background-color: #3e8e41;}
+		ul.topnav li a:hover:not(.active) {background-color: #333;}
 
 		ul.topnav li a.active {background-color: #333;}
 
 		ul.topnav li.right {float: right;}
 
-		@media screen and (max-width: 600px) {
+		/* @media screen and (max-width: 600px) {
 			ul.topnav li.right, 
 			ul.topnav li {float: none;}
+		} */
+
+		/* Style the input container */
+		.input-container {
+		display: flex;
+		width: 100%;
+		margin-bottom: 15px;
+		}
+
+		/* Style the form icons */
+		.icon {
+		padding: 10px;
+		background: #1E90FF;
+		color: white;
+		min-width: 50px;
+		text-align: center;
+		}
+
+		/* Style the input fields */
+		.input-field {
+		width: 100%;
+		padding: 10px;
+		outline: none;
+		}
+
+		.input-field:focus {
+		border: 2px solid #1E90FF;
+		}
+
+		/* Set a style for the submit button */
+		.btn {
+		background-color: #1E90FF;
+		color: white;
+		padding: 10px 20px;
+		border: none;
+		cursor: pointer;
+		width: 100%;
+		opacity: 0.9;
+		}
+
+		.btn:hover {
+		opacity: 1;
 		}
 		</style>
 		
@@ -63,73 +103,65 @@
 	
 	<body>
 
-		<h2 align="center">Vendor Machine User Database</h2>
+		<br>
 		<ul class="topnav">
 			<li><a href="home.php">Home</a></li>
 			<li><a href="user data.php">User Data</a></li>
 			<li><a class="active" href="registration.php">Registration</a></li>
 			<!-- <li><a href="read tag.php">Scan Card</a></li> -->
+			<li><a href="live_chart.php">Live Data</a></li>
 		</ul>
+		<h2 align="center">Vending Machine User Registration</h2>
 
 		<div class="container">
 			<br>
-			<div class="center" style="margin: 0 auto; width:495px; border-style: solid; border-color: #f2f2f2;">
+			<div style="margin: auto; max-width:500px; border-style: solid; border-color: #1E90FF">
 				<div class="row">
 					<h3 align="center">Registration Form</h3>
 				</div>
 				<br>
-				<form class="form-horizontal" action="insertDB.php" method="post" >
-					<div class="control-group">
-						<label class="control-label">ID</label>
-						<div class="controls">
-							<textarea name="id" id="getUID" placeholder="Please Scan your Card / Key Chain to display ID" rows="1" cols="1" required></textarea>
-						</div>
-					</div>
-					
-					<div class="control-group">
-						<label class="control-label">Name</label>
-						<div class="controls">
-							<input id="div_refresh" name="name" type="text"  placeholder="" required>
-						</div>
+				<form action="insertDB.php" style="max-width:400px; margin:auto" method="post" >
+					<div class="input-container">
+						<i class="fa fa-id-badge icon"></i>
+						<input class="input-field" type="text" placeholder="Card ID No" name="id" required>
 					</div>
 
-					<div class="control-group">
-						<label class="control-label">NID / Birth Certificate</label>
-						<div class="controls">
-							<input name="nid" type="text"  placeholder="" required>
-						</div>
+					<div class="input-container">
+						<i class="fa fa-user icon"></i>
+						<input class="input-field" type="text" placeholder="Full Name" name="name" required>
 					</div>
+
+					<div class="input-container">
+						<i class="fa fa-id-card icon"></i>
+						<input class="input-field" type="text" placeholder="NID / Birth Certificate No" name="nid" required>
+					</div>
+
 					
-					<div class="control-group">
-						<label class="control-label">Gender</label>
-						<div class="controls">
-							<select name="gender">
+					<div class="input-container">
+
+							<i class="fa fa-venus-mars icon"></i>
+							<select name="gender" >
 								<option value="Male">Male</option>
 								<option value="Female">Female</option>
 								<option value="Female">Other</option>
 							</select>
-						</div>
 					</div>
 					
-					<div class="control-group">
-						<label class="control-label">No of Member</label>
-						<div class="controls">
-							<input name="member" type="int" placeholder="" required>
-						</div>
+					<div class="input-container">
+					<i class="fa fa-calculator icon"></i>
+					<input class="input-field" type="text" placeholder="No of Family Member" name="member" required>
 					</div>
 					
-					<div class="control-group">
-						<label class="control-label">Address</label>
-						<div class="controls">
-							<input name="address" type="text"  placeholder="" required>
-						</div>
+					<div class="input-container">
+						<i class="fa fa-address-book icon"></i>
+						<input class="input-field" type="text" placeholder="Address" name="address" required>
 					</div>
 					
-					<div class="form-actions">
-						<button type="submit" class="btn btn-success">Submit</button>
-                    </div>
+					
+					<button type="submit" class="btn">Register</button>
+                
 				</form>
-				
+				<br>
 			</div>               
 		</div> <!-- /container -->	
 	</body>
