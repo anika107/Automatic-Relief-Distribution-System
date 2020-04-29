7a81b1e759dfc05572c6abf1660eaab7cc1aefd2
alpha05 <fzn.alpha05@gmail.com>
2020-04-30 00:29:25 +0600

    updated registration and insert

--- php/insertDB.php
+++ php/insertDB.php
@@ -3,23 +3,22 @@
     require 'database.php';
  
     if ( !empty($_POST)) {
-        // keep track post values
-		$id = $_POST['id'];
 		$name = $_POST['name'];
 		$nid = $_POST['nid'];
 		$gender = $_POST['gender'];
-        $member = $_POST['member'];
-		$address = $_POST['address'];
-		
+        $member = $_POST['family_size'];
+		$upazilla = $_POST['upazilla'];
+		$district = $_POST['district'];
+		$division = $_POST['division'];
         
 		// insert data
         $pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO info (id,name,nid,gender,member,address)
-    values ('$id','$name','$nid','$gender','$member','$address')";
+		$sql = "INSERT INTO info (id,name,nid,gender,family_size,upazilla,district,division)
+    values ('$id','$name','$nid','$gender','$member','$upazilla','$district','$division')";
 		$q = $pdo->prepare($sql);
-		$q->execute(array($id,$name,$nid,$gender,$member,$address));
+		$q->execute(array($id,$name,$nid,$gender,'$member','$upazilla','$district','$division'));
 		Database::disconnect();
-		header("Location: user data.php");
+		header("Location: ./user_data.php");
     }
 ?>
--- php/registration.php
+++ php/registration.php
@@ -1,14 +1,36 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
-<html lang="en">
-<html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-		
-		<style>
-		
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gym Template">
+    <meta name="keywords" content="Gym, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> User Registration | PS </title>
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" >
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Didact+Gothic"/>
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/barfiller.css">
+    <link rel="stylesheet" type="text/css" href="../css/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="../css/slicknav.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/default.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="../css/fonts.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="../css/cssTable/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/csstable/main.css">  
+    
+    <style>
 		html {
 			font-family: Arial;
 			display: inline-block;
@@ -96,73 +118,160 @@
 		opacity: 1;
 		}
 		</style>
-		
-		<title>Registration</title>
-	</head>
-	
-	<body>
+</head>
 
-		<br>
-		<ul class="topnav">
-			<li><a href="home.php">Home</a></li>
-			<li><a href="user data.php">User Data</a></li>
-			<li><a class="active" href="registration.php">Registration</a></li>
-			<!-- <li><a href="read tag.php">Scan Card</a></li> -->
-			<li><a href="live_chart.php">Live Data</a></li>
-			<input type="text" placeholder="Search">
-		</ul>
-		<h2 align="center">Vending Machine User Registration</h2>
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
 
-		<div class="container">
-			<br>
-			<div style="margin: auto; max-width:500px; border-style: solid; border-color: #1E90FF">
-				<div class="row">
-					<h3 align="center">Registration Form</h3>
-				</div>
-				<br>
-				<form action="insertDB.php" style="max-width:400px; margin:auto" method="post" >
-					<div class="input-container">
-						<i class="fa fa-id-badge icon"></i>
-						<input class="input-field" type="text" placeholder="Card ID No" name="id" required>
-					</div>
+    <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="logo">
+                        <a href="./index.html">
+                            <img src="../template/img/logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <nav class="nav-menu">
+                        <?php if (isset($_SESSION['logged']) && $_SESSION['username'] == "admin") { ?>
+                            <ul>
+                                <li><a href="./index.php">Home</a></li>
+                                <li><a href="#">Relief Card</a>
+                                    <ul class="dropdown">
+                                        <li class="active"><a href="./registration.php">New Registration</a></li>
+                                        <li><a href="./user_data.php">User Information</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="./live_relief.php">Relief Distribution Data</a></li>
+                                <li><a href="./live_covid19.php">Covid19 Update</a></li>
+                                <li><a href="./logout.php">Log Out</a></li>
+                            </ul>
+							
+                        <?php } else { ?> 
+							<ul>
+                                <li><a href="./index.php">Home</a></li>
+                                <li><a href="./live_relief.php">Relief Distribution Data</a></li>
+                                <li><a href="./live_covid19.php">Covid19 Update</a></li>
+                                <li><a href="./login.php">Admin Log In</a></li>
+                            </ul>
+						<?php } ?> 
+                        
+                    </nav>
+                </div>
+                <div class="col-lg-3">
+                    <div class="top-option">
+                        <div class="to-search search-switch">
+                            <i class="fa fa-search"></i>
+                        </div>
+                        <div class="to-social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="canvas-open">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="../image/card9.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb-text">
+                        <h2>Vending Machine User Registration</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+    
+    <!-- Table -->
+    	<div id="wrapper">
+            <div id="three-column" class="container">
+				<div class="title">
+					<p style="font-size: 24px;color: #757575; padding-top: 20px;">Please input correct data in the fields.</p>
+				</div>		
+            </div>	
+            <div style="margin: auto; max-width:500px; border-style: solid; border-color: #1E90FF">
+                <div class="row">
+                    <h3 align="center" style="padding-left: 160px;">Registration Form</h3>
+                </div>
+                <br>
+                <form action="insertDB.php" style="max-width:400px; margin:auto" method="post" >
+                    <div class="input-container">
+                        <i class="fa fa-user icon"></i>
+                        <input class="input-field" type="text" placeholder="Full Name" name="name" required>
+                    </div>
+                    <div class="input-container">
+                        <i class="fa fa-id-card icon"></i>
+                        <input class="input-field" type="text" placeholder="NID / Birth Certificate No" name="nid" required>
+                    </div>
+                    <div class="input-container">
+                        <i class="fa fa-venus-mars icon"></i>
+                            <select name="gender" >
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Female">Other</option>
+                            </select>
+                    </div>
+                    <div class="input-container">
+                    <i class="fa fa-calculator icon"></i>
+                    <input class="input-field" type="text" placeholder="No of Family Member" name="member" required>
+                    </div>
+                    <div class="input-container">
+                        <i class="fa fa-address-book icon"></i>
+                        <input class="input-field" type="text" placeholder="Upazilla" name="upazilla" required>
+                    </div>
+                    <div class="input-container">
+                        <i class="fa fa-address-book icon"></i>
+                        <input class="input-field" type="text" placeholder="District" name="district" required>
+                    </div>
+                    <div class="input-container">
+                        <i class="fa fa-address-book icon"></i>
+                        <input class="input-field" type="text" placeholder="Division" name="division" required>
+                    </div>
+                    <button type="submit" class="btn">Register</button>
+                </form>
+                <br>
+            </div> 
+        </div>
+     
+    <!-- Footer Section Begin -->
+    <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="copyright-text">
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer Section End -->
 
-					<div class="input-container">
-						<i class="fa fa-user icon"></i>
-						<input class="input-field" type="text" placeholder="Full Name" name="name" required>
-					</div>
+    <!-- Js Plugins -->
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.magnific-popup.min.js"></script>
+    <script src="../js/masonry.pkgd.min.js"></script>
+    <script src="../js/jquery.barfiller.js"></script>
+    <script src="../js/jquery.slicknav.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/main.js"></script>
 
-					<div class="input-container">
-						<i class="fa fa-id-card icon"></i>
-						<input class="input-field" type="text" placeholder="NID / Birth Certificate No" name="nid" required>
-					</div>
-
-					
-					<div class="input-container">
-
-							<i class="fa fa-venus-mars icon"></i>
-							<select name="gender" >
-								<option value="Male">Male</option>
-								<option value="Female">Female</option>
-								<option value="Female">Other</option>
-							</select>
-					</div>
-					
-					<div class="input-container">
-					<i class="fa fa-calculator icon"></i>
-					<input class="input-field" type="text" placeholder="No of Family Member" name="member" required>
-					</div>
-					
-					<div class="input-container">
-						<i class="fa fa-address-book icon"></i>
-						<input class="input-field" type="text" placeholder="Address" name="address" required>
-					</div>
-					
-					
-					<button type="submit" class="btn">Register</button>
-                
-				</form>
-				<br>
-			</div>               
-		</div> <!-- /container -->	
-	</body>
+</body>
 </html>
